9f86bb1dbf77a05ae765dd3e76c70987819fd987
James Barry <james.barry26@mail.dcu.ie>
2023-07-19 05:37:09 -0400

    Update download link to Lindat repository

--- scripts/download_iwpt_data.sh
+++ scripts/download_iwpt_data.sh
@@ -3,5 +3,13 @@
 # 2020 data (should also be available on Lindat): http://ufal.mff.cuni.cz/~zeman/soubory/iwpt2020-train-dev.tgz
 
 cd data
-wget https://ufal.mff.cuni.cz/~zeman/soubory/iwpt2021-train-dev.tgz
-tar -xvzf iwpt2021-train-dev.tgz
+
+mkdir -p iwpt2021stdata
+cd iwpt2021stdata
+
+curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3728{/iwpt2021stdata.tgz}
+tar -xf iwpt2021stdata.tgz
+mkdir ud_treebanks_iwpt
+mv UD_* ud_treebanks_iwpt/
+
+echo "Done"
